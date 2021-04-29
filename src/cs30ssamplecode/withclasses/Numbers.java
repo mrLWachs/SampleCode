764e6e7a2ea1c5f55d35772bc360c3d40f247f39
Mr. Wachs <mrLWachs@gmail.com>
2021-04-29 09:07:05 -0500

    Update Numbers.java

--- src/cs30ssamplecode/withclasses/Numbers.java
+++ src/cs30ssamplecode/withclasses/Numbers.java
@@ -14,8 +14,32 @@ public class Numbers
 
     // Encapsulated global class properties below..............................
     
-    private static final char   NEGATIVE = '-';
-    private static final char   DECIMAL  = '.';
+    private static final char NEGATIVE = '-';
+    private static final char DECIMAL  = '.';
+    
+    // Public and static (shared, does not an object) methods below............ 
+    
+    /**
+     * Checks the value to see if it contains numerical characters or a "-"
+     * 
+     * @param value the string of characters to check
+     * @return is a number (true) or not (false)
+     */
+    public static boolean isInteger(String value) {
+        // Convert string into array of char values (built-in string method)
+        char[] characters = value.toCharArray();
+        // Loop (traverse) through this array from start to end
+        for (int i = 0; i < characters.length; i++) {    
+            // Use built-in method to see if this character is a digit (0-9)
+            if (Character.isDigit(characters[i]) == false) {
+                // Character was not a digit, but is it the negative sign 
+                if (characters[i] != NEGATIVE) {
+                    return false;       // Not a valid character for a integer
+                }
+            }
+        }
+        return true;                    // All characters valid for a integer
+    }
     
     /**
      * Checks the value to see if it contains numerical characters or a "-" 
@@ -25,36 +49,23 @@ public class Numbers
      * @return is a number (true) or not (false)
      */
     public static boolean isDouble(String value) {
+        // Convert string into array of char values (built-in string method)
         char[] characters = value.toCharArray();
-        for (int i = 0; i < characters.length; i++) {            
+        // Loop (traverse) through this array from start to end
+        for (int i = 0; i < characters.length; i++) {         
+            // Use built-in method to see if this character is a digit (0-9)
             if (Character.isDigit(characters[i]) == false) {
+                // Character was not a digit, but is it the negative sign or
+                // the decimal symbol
                 if (characters[i] != DECIMAL && 
                     characters[i] != NEGATIVE) {
-                    return false;
+                    return false;       // Not a valid character for a double
                 }
             }
         }
-        return true;
+        return true;                    // All characters valid for a double
     }
     
-    /**
-     * Checks the value to see if it contains numerical characters or a "-"
-     * 
-     * @param value the string of characters to check
-     * @return is a number (true) or not (false)
-     */
-    public static boolean isInteger(String value) {
-        char[] characters = value.toCharArray();
-        for (int i = 0; i < characters.length; i++) {            
-            if (Character.isDigit(characters[i]) == false) {
-                if (characters[i] != NEGATIVE) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
     /**
      * Determines if a number if odd or even
      * 
@@ -62,8 +73,8 @@ public class Numbers
      * @return true (if even), false (if odd)
      */
     public static boolean isEven(int number) {
-        if (number % 2 == 0) return true;
-        else                 return false;
+        if (number % 2 == 0) return true;   // Number divides evenly by zero
+        else                 return false;  // Number does not divide by zero
     }
     
     /**
@@ -93,27 +104,27 @@ public class Numbers
     }
     
     /**
-     * Generate a random number in a range
+     * Generate a random number (a double) in a range
      * 
-     * @param low the lowest number in the range
-     * @param high the highest number in the range
-     * @return random number in the range
+     * @param low the lowest double in the range
+     * @param high the highest double in the range
+     * @return random double in the range
+     */
+    public static double random(double low, double high) {
+        return ((high - low + 1d) * Math.random() + low);
+    }
+    
+    /**
+     * Generate a random number (an integer) in a range
+     * 
+     * @param low the lowest integer in the range
+     * @param high the highest integer in the range
+     * @return random integer in the range
      */
     public static int random(int low, int high) {
         return (int)(random((double)low, (double)high));
     }
     
-    /**
-     * Generate a random number in a range
-     * 
-     * @param low the lowest number in the range
-     * @param high the highest number in the range
-     * @return random number in the range
-     */
-    public static double random(double low, double high) {
-        return ((high - low + 1d) * Math.random() + low);
-    }
-
     /**
      * Generates an array of random integers in the range between low and high
      * 
@@ -123,11 +134,11 @@ public class Numbers
      * @return an array of random integers
      */
     public static int[] random(int low, int high, int size) {
-        int[] numbers = new int[size];      // create empty array of passed size
-        for (int i = 0; i < size; i++) {    // traverse array size
-            numbers[i] = random(low,high);  // assign random value to each index
+        int[] numbers = new int[size];      // Create empty array of passed size
+        for (int i = 0; i < size; i++) {    // Traverse array 
+            numbers[i] = random(low,high);  // Assign random value to each index
         }
-        return numbers;                     // return completed array
+        return numbers;                     // Return completed array
     }
     
     /**
@@ -139,11 +150,11 @@ public class Numbers
      * @return an array of random doubles
      */
     public static double[] random(double low, double high, int size) {
-        double[] numbers = new double[size]; // create empty array 
-        for (int i = 0; i < size; i++) {    // traverse array size
-            numbers[i] = random(low,high);  // assign random value to each index
+        double[] numbers = new double[size]; // Create empty array 
+        for (int i = 0; i < size; i++) {    // Traverse array 
+            numbers[i] = random(low,high);  // Assign random value to each index
         }
-        return numbers;                     // return completed array
+        return numbers;                     // Return completed array
     }
    
     /**
@@ -156,11 +167,11 @@ public class Numbers
      * @return a matrix of random integers
      */
     public static int[][] random(int rows, int columns, int low, int high) {
-        int[][] matrix = new int[rows][columns];    // create empty matrix
-        for (int row = 0; row < rows; row++) {      // traverse rows
-            matrix[row] = random(low, high, columns);   // create random row
+        int[][] matrix = new int[rows][columns];    // Create empty matrix
+        for (int row = 0; row < rows; row++) {      // Traverse rows
+            matrix[row] = random(low, high, columns);   // Create random row
         }
-        return matrix;                              // return completed matrix
+        return matrix;                              // Return completed matrix
     }
     
     /**
@@ -174,11 +185,11 @@ public class Numbers
      */
     public static double[][] random(int rows, int columns, double low, 
             double high) {
-        double[][] matrix = new double[rows][columns];    // create empty matrix
-        for (int row = 0; row < rows; row++) {      // traverse rows
-            matrix[row] = random(low, high, columns);   // create random row
+        double[][] matrix = new double[rows][columns];    // Create empty matrix
+        for (int row = 0; row < rows; row++) {      // Traverse rows
+            matrix[row] = random(low, high, columns);   // Create random row
         }
-        return matrix;                              // return completed matrix
+        return matrix;                              // Return completed matrix
     }
     
 }
